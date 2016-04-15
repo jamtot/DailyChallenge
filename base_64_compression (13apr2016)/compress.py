bad2cc2e2be9e3725c52d21f9761260e676c2560
jamtot <jongrammor@gmail.com>
2016-04-15 18:07:01 +0100

    Got it decoding, just by reversing the weird encoding process I had going.

--- base_64_compression (13apr2016)/compress.py
+++ base_64_compression (13apr2016)/compress.py
@@ -7,10 +7,12 @@ base64indextable="""ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567
 
 padding_symbol = '='
 
-input2="""Man is distinguished, not only by his reason, but by this singular passion from
+to_encode="""Man is distinguished, not only by his reason, but by this singular passion from
 other animals, which is a lust of the mind, that by a perseverance of delight
 in the continued and indefatigable generation of knowledge, exceeds the short
 vehemence of any carnal pleasure."""
+
+to_decode="""TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbQpvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodAppbiB0aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdlLCBleGNlZWRzIHRoZSBzaG9ydAp2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4="""
 
 def encode(input_text, index_table, padder):
     output = ""
@@ -31,7 +33,26 @@ def encode(input_text, index_table, padder):
     output+=padder*pads
 
     return output
+
+def decode(encoded_text, index_table, padder):
+    output = ""
+    if (len(encoded_text)%4!=0):
+        print "Not a valid input, compadre."
+    else:
+        for i in xrange(0, len(encoded_text), 4):
+            curbin = ""
+
+            for l in encoded_text[i:i+4]:
+                if l != padder:
+                    binletter6 = "{0:b}".format(index_table.index(l)).zfill(6)
+                    curbin+=binletter6
+
+            for i in xrange(0, len(curbin), 8):
+                output+=chr(int(curbin[i: i+8], 2))
+
+    return output
         
 if __name__ == "__main__":
-    print encode(input2, base64indextable, padding_symbol)
+    print encode(to_encode, base64indextable, padding_symbol)
+    print decode(to_decode, base64indextable, padding_symbol)
     
