6da9f840cad3349359b7d088b8a88be1dd15e88b
jamtot <jongrammor@gmail.com>
2016-04-16 16:20:44 +0100

    Changed around the matching to get consonants at the start of a word, and added text to speech. Ayy lmao.

--- name_game (20apr2015)/naemgaem.py
+++ name_game (20apr2015)/naemgaem.py
@@ -1,4 +1,11 @@
 import re
+import subprocess
+
+#got from http://stackoverflow.com/a/19058023/5991253
+def execute_unix(inputcommand):
+   p = subprocess.Popen(inputcommand, stdout=subprocess.PIPE, shell=True)
+   (output, err) = p.communicate()
+   return output
 
 def name_game(name):
     if len(name)<2: return "Stop that!"
@@ -9,22 +16,26 @@ def name_game(name):
     #makes sure name is displayed nicely
     name = name[0].upper()+name[1:].lower()
     
-    return """%s, %s bo%s,
+    return """%s %s bo%s,
 Bonana fanna fo%s,
 Fee fy mo%s,
 %s!"""%(name,name,change(name,"B"),change(name,"F"),change(name,"M"),name)
 
 def change(name,letter):
-    
     if name.startswith(letter):
         return "-"+name[1:]
-    # add space to stop losing the front if vowel
-    name = re.sub(r"[AEIOU].*", " "+name.lower(), name)
-    return " "+letter+name[1:]
+    if re.match(r"[AEIOU].*",name): name = name.lower()
+    else:
+        # remove front consonants
+        while re.match(r"[^aeiouy].*",name):
+            name=name[1:]
+    return " "+letter+name
 
 if __name__ == "__main__":
     print "Ctrl-Z to close."
     while True:
         print "Enter name to play the game: "
-        print name_game(raw_input())
+        gaem = name_game(raw_input())
+        print gaem
+        execute_unix("espeak '%s'"%gaem)
         print
